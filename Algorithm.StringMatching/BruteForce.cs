ec63b4c440747651b101599cd2daf29e262cc0f2
hrpqq <hrpqq@outlook.com>
2022-11-02 11:24:51 +0800

    add string matching projext

---
+++ Algorithm.StringMatching/BruteForce.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithm.StringMatching
+{
+    public class BruteForce
+    {
+        public static int MatchFirst(string source, string pattern)
+        {
+            int returnVal = -1;
+            int sl, pl;
+            sl = source.Length;
+            pl = pattern.Length;
+            if (sl >= pl && pl > 0)
+            {
+                bool find = false;
+                for (int i = 0; i < sl - pl; i++)
+                {
+                    for (int j = 0; j < pl; j++)
+                    {
+                        if (source[i + j] != pattern[j])
+                            break;
+                        else   
+                        {
+                            if (j == pl - 1)
+                            {
+                                returnVal = i;
+                                find = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (find) break;
+                }
+            }
+            return returnVal;
+        }
+    }
+}
---
+++ Algorithm.Test/StringMatching/BruteForceTest.cs
@@ -0,0 +1,42 @@
+﻿using Algorithm.Sort;
+using Algorithm.StringMatching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Test.StringMatching
+{
+    [TestClass]
+    public class BruteForceTest
+    {
+        [TestMethod]
+        public void shouldFindOneMatchingAtIndexSeven()
+        {
+            string source = "abccbaddnbfq";
+            string pattern = "dnb";
+            var index = BruteForce.MatchFirst(source, pattern);
+            Assert.AreNotEqual(-1, index);
+            Assert.AreEqual(7, index);
+        }
+
+        [TestMethod]
+        public void shouldFindOneMatchingAtIndexZero()
+        {
+            string source = "abccbaddnbfq";
+            string pattern = "abc";
+            var index = BruteForce.MatchFirst(source, pattern);
+            Assert.AreEqual(0, index);
+        }
+
+        [TestMethod]
+        public void shouldFindNoMatching()
+        {
+            string source = "abccbaddnbfq";
+            string pattern = "dns";
+            var index = BruteForce.MatchFirst(source, pattern);
+            Assert.AreEqual(-1, index);
+        }
+    }
+}
