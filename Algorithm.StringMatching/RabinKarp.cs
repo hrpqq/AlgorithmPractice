ceeee4c231eac584a133a398df7e043101871564
hrpqq <hrpqq@outlook.com>
2022-11-03 14:08:34 +0800

    complete RabinKarp and its test

---
+++ Algorithm.StringMatching/RabinKarp.cs
@@ -0,0 +1,142 @@
+﻿using System.Text;
+
+namespace Algorithm.StringMatching
+{
+    public class RabinKarp
+    {
+        public const uint PRIME_NUM = 9973;
+        public static int MatchFirst(string source, string pattern)
+        {
+            int result = -1;
+            if (source.Length >= pattern.Length && pattern.Length > 0)
+            {
+                int patternLength = pattern.Length;
+                uint? patternHash = GetStringHashCode(pattern);
+
+                string lastWindowString = String.Empty;
+                uint? lastWindowHash = null;
+                for (int i = 0; i < source.Length - patternLength; i++)
+                {
+                    var curWindowString = source.Substring(i, patternLength);
+                    uint? curWindowHash = lastWindowHash == null ? GetStringHashCode(curWindowString) : null;
+
+                    if (lastWindowHash != null
+                        && !string.IsNullOrEmpty(lastWindowString))
+                    {
+                        curWindowHash = GetRollingHashCode(lastWindowString,
+                            (source[i + patternLength - 1].ToString(), lastWindowHash.Value));
+                    }
+                    if (curWindowHash == patternHash
+                        && pattern == curWindowString)
+                    {
+                        result = i;
+                        break;
+                    }
+
+                    lastWindowString = curWindowString;
+                    lastWindowHash = curWindowHash;
+                }
+            }
+            return result;
+        }
+
+        public static uint? GetRollingHashCode(string prevStr, (string nextChar, uint prevHash) additional)
+        {
+            var prevBytes = String2Bytes(prevStr);
+            var prevHeadBytes = String2Bytes(prevStr.Substring(0, 1));
+            var additional4Bytes = (String2Bytes(additional.nextChar), additional.prevHash);
+            return GetBytesRollingHashCode(prevBytes, prevHeadBytes, additional4Bytes);
+        }
+
+
+        public static uint? GetBytesRollingHashCode(byte[] prevBytes, byte[] prevHeadBytes, (byte[] nextBytes, uint prevBytesHash) additional)
+        {
+            uint? result;
+            if (additional == default((byte[] nextBytes, uint prevHashCode)))
+            {
+                result = GetBytesHashCode(prevBytes);
+            }
+            else
+            {
+                int baseBytesLength = prevBytes.Length - prevHeadBytes.Length;
+                var baseHash = GetBytesHashCode(CreateBaseBytes(baseBytesLength));
+                var headValue = ParseBytes2Uint(prevHeadBytes);
+
+                var headHash = (headValue * baseHash % PRIME_NUM);
+                var restHash = additional.prevBytesHash >= headHash 
+                                ? additional.prevBytesHash - headHash
+                                : PRIME_NUM - headHash + additional.prevBytesHash;
+
+                var nextBytesHash = GetBytesHashCode(additional.nextBytes);
+                var nextBaseValue = ParseBytes2Uint(CreateBaseBytes(additional.nextBytes.Length));
+
+                var newHash = (restHash * nextBaseValue + nextBytesHash) % PRIME_NUM;
+                result = newHash;
+            }
+            return result;
+        }
+
+        public static uint? GetStringHashCode(string s)
+        {
+            if (s.Length > 0)
+                return GetBytesHashCode(String2Bytes(s));
+            return null;
+        }
+
+        public static uint? GetBytesHashCode(byte[] bytes)
+        {
+            uint? result = null;
+            uint GetBaseHash(int count)
+            {
+                ulong tempHash = 1;
+                ulong baseValue = 1ul << 32;
+                for (int i = 0; i < count; i++)
+                {
+                    tempHash = tempHash * baseValue % PRIME_NUM;
+                }
+                return (uint)tempHash;
+            }
+
+            if (bytes.Length > 0)
+            {
+                uint prevHash = 0;
+                for (int i = bytes.Length - 1, j = 0; i >= 0; i -= 4, j++)
+                {
+                    ulong curVal = (
+                                bytes[i]
+                                | (i - 1 >= 0 ? (((uint)bytes[i - 1]) << 8) : 0)
+                                | (i - 2 >= 0 ? (((uint)bytes[i - 2]) << 16) : 0)
+                                | (i - 3 >= 0 ? (((uint)bytes[i - 3]) << 24) : 0)
+                               );
+                    prevHash = (uint)((curVal * GetBaseHash(j) + prevHash) % PRIME_NUM);
+                }
+                result = prevHash;
+            }
+            return result;
+        }
+
+        private static uint? ParseBytes2Uint(byte[] bytes)
+        {
+            if (bytes.Length > 0)
+            {
+                int i = bytes.Length - 1;
+                return bytes[i]
+                        | (i - 1 >= 0 ? (((uint)bytes[i - 1]) << 8) : 0)
+                        | (i - 2 >= 0 ? (((uint)bytes[i - 2]) << 16) : 0)
+                        | (i - 3 >= 0 ? (((uint)bytes[i - 3]) << 24) : 0);
+            }
+            return null;
+        }
+
+        private static byte[] String2Bytes(string s)
+        {
+            return Encoding.UTF8.GetBytes(s);
+        }
+
+        private static byte[] CreateBaseBytes(int repeat)
+        {
+            return new byte[] { 1 }.Concat(Enumerable.Repeat<byte>(0, repeat)).ToArray();
+        }
+        
+    }
+}
---
+++ Algorithm.Test/StringMatching/RabinKarpTest.cs
@@ -0,0 +1,68 @@
+﻿using Algorithm.StringMatching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Test.StringMatching
+{
+    [TestClass]
+    public class RabinKarpTest
+    {
+        [TestMethod]
+        public void shouldFindOneMatchingAtIndexSeven()
+        {
+            string source = "abccbaddnbfq";
+            string pattern = "dnb";
+            var index = RabinKarp.MatchFirst(source, pattern);
+            Assert.AreNotEqual(-1, index);
+            Assert.AreEqual(7, index);
+        }
+
+        [TestMethod]
+        public void shouldGenerateHashValue4Bytes()
+        {
+            byte[] inputBytes = new byte[] { 45, 22, 15, 99, 34, 0 };
+            byte[] inputBytes2 = new byte[] { 0, 0, 45, 22, 15, 99, 34, 0 };
+            var hash = RabinKarp.GetBytesHashCode(inputBytes);
+            var hash2 = RabinKarp.GetBytesHashCode(inputBytes2);
+            Assert.AreEqual(hash, hash2);
+
+
+            inputBytes = new byte[] { 45, 22, 0, 0, 0, 1 };
+            inputBytes2 = new byte[] { 0, 0, 45, 22, 0, 0, 0, 1 };
+            hash = RabinKarp.GetBytesHashCode(inputBytes);
+            hash2 = RabinKarp.GetBytesHashCode(inputBytes2);
+            Assert.AreEqual(hash, hash2);
+        }
+
+        [TestMethod]
+        public void shouldGenerateHashValue4String()
+        {
+            string inputBytes = "abcddbauf";
+            string inputBytes2 = Encoding.UTF8.GetString(new byte[] { 0 }) + "abcddbauf";
+            var hash = RabinKarp.GetStringHashCode(inputBytes);
+            var hash2 = RabinKarp.GetStringHashCode(inputBytes2);
+            Assert.AreEqual(hash, hash2);
+        }
+
+        [TestMethod]
+        public void shouldGenerateRollingHash4Bytes()
+        {
+            byte[] prevBytes = new byte[] { 2, 45, 22, 0, 0, 0 };
+            var hash1 = RabinKarp.GetBytesHashCode(prevBytes);
+
+            byte[] curBytes = new byte[] { 45, 22, 0, 0, 0, 1 };
+            var hashc = RabinKarp.GetBytesHashCode(curBytes);
+
+            var hashRolling = RabinKarp.GetBytesRollingHashCode(new byte[] { 2, 45, 22, 0, 0, 0 }, new byte[] { 2 },
+                (new byte[] { 1 }, hash1.Value));
+
+            Assert.AreEqual(hashc, hashRolling);
+        }
+
+    }
+}
